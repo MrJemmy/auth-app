69e83d62a1159147b53ce2d61215973aefd6aaf1
MrJemmy <jbpatel1411@gmail.com>
2025-05-14 17:05:39 +0530

    auth file created

--- server/src/middleware/verifyRoles.js
+++ server/src/middleware/verifyRoles.js
@@ -1,8 +1,11 @@
 const verifyRoles = (...allowedRoles) => {
     return (req, res, next) => {
+
+        // if product is created by user then only alow user
+
         if (!req?.user?.roles) return res.status(401).json({ msg: "Unauthorized User" })
         const result = req.user.roles.map(role => allowedRoles.includes(role)).some(val => val === true)
-        if(!result) return res.status(401).json({ msg: "Unauthorized User"})
+        if (!result) return res.status(401).json({ msg: "Unauthorized User" })
         next()
     }
 }
--- server/src/user/routes.js
+++ server/src/user/routes.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const { authToken } = require("../middleware/tokenAuth")
 const user = require("./controllers")
+const userAuth = require("./auth")
 const verifyRoles = require("../middleware/verifyRoles")
 const { ROLES_LIST } = require("./const")
 const { profilePic } = require("../config/multer_config")
@@ -11,17 +12,16 @@ const route = express.Router()
 // route.use(authToken)  // this will apply auth on all below API
 
 
-route.post('/register', profilePic.single("image"), user.register)
-route.post('/login', user.login)
-route.post('/refresh', user.refreshToken);
-route.post('/generate_otp', user.generateOTP);
-route.post('/verify_otp', user.verifyOTP);
-route.post('/forgot_password', user.forgotPassword);
-route.post('/reset_password', authToken, user.resetPassword);
-route.post('/logout', user.logout);
+route.post('/register', profilePic.single("image"), userAuth.register)
+route.post('/login', userAuth.login)
+route.post('/refresh', userAuth.refreshToken);
+route.post('/generate_otp', userAuth.generateOTP);
+route.post('/verify_otp', userAuth.verifyOTP);
+route.post('/forgot_password', userAuth.forgotPassword);
+route.post('/reset_password', authToken, userAuth.resetPassword);
+route.post('/logout', userAuth.logout);
 
 route.get('/', authToken, verifyRoles(ROLES_LIST.admin, ROLES_LIST.editor), user.getAll)
-
 route.get('/:userId', authToken, user.getOne)
 route.put('/:user_id', authToken, user.updateOne)
 route.delete('/:user_id', authToken, user.deleteOne)
